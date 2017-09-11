35505a054a37644774485e742defda55e69886a8
Connor Trotter <connor@connors-mbp.attlocal.net>
2017-09-11 18:07:59 -0400

    mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 function Controls(){
   this.Create = function(){
-    var p = new Point(random(width - 100) + 50, random(height - 100) + 50, 1000);
+    var p = new Point(random(width - 100) + 50, random(height - 100) + 50, width/1.5);
     points[points.length] = p;
     var f = gui.addFolder(points.length);
     f.add(p, "x").listen();
@@ -35,11 +35,12 @@ function setup(){
   gui.add(ctrl, "Save");
 
   for(var i = 0; i < 3; i++){
-    // points[i] = new Point(random(width - 100) + 50, random(height - 100) + 50, 1000);
     ctrl.Create();
   }
 
   dat.GUI.toggleHide();
+
+  console.log(width/80);
 }
 
 function draw(){
@@ -47,23 +48,32 @@ function draw(){
   clear();
 
   for(let p of points){
+    //move points
     if(!alreadySelected &&
        mouseIsPressed &&
        mouseX < p.x + 80 &&
        mouseX > p.x - 80 &&
        mouseY < p.y + 80 &&
        mouseY > p.y - 80){
+      //if a point has already been selected, don't move another
       alreadySelected = true;
+      //move currently selected point to the top
       var i = points.indexOf(p);
       points.splice(i, 1);
       points.unshift(p);
       p.x = mouseX;
       p.y = mouseY;
+
+      if(touches.length > 1){
+        p.radius = dist(touches[0].x, touches[0].y, touches[touches.length-1].x, touches[touches.length-1].y);
+      }
     }
   }
+  //display points from bottom to top
   for(var i = points.length-1; i >=0; i--){
-    fill(points[i].x / width * 360, points[i].y / height * 100, 95, 0.5);
+    fill(points[i].x / width * 360, points[i].y / height * 100, 95, 0.3);
     ellipse(points[i].x, points[i].y, points[i].radius);
+    //TODO: make centers more visible
     fill(0, 0, 100, 1);
     ellipse(points[i].x, points[i].y,  10);
   }
@@ -74,22 +84,35 @@ function draw(){
 var lastTouchY = 0;
 var preventPullToRefresh = false;
 
+$('.js-plus').on('click', function(e){
+  e.preventDefault();
+  ctrl.Create();
+});
+
+$('.js-plus').on('tap', function(e){
+  e.preventDefault();
+  ctrl.Create();
+});
+
 $('body').on('touchstart', function (e) {
-    if (e.originalEvent.touches.length != 1) { return; }
-    lastTouchY = e.originalEvent.touches[0].clientY;
-    preventPullToRefresh = window.pageYOffset == 0;
+    // if (e.originalEvent.touches.length != 1) { return; }
+    // lastTouchY = e.originalEvent.touches[0].clientY;
+    // preventPullToRefresh = window.pageYOffset == 0;
+
+    // e.preventDefault();
 });
 
 $('body').on('touchmove', function (e) {
-    var touchY = e.originalEvent.touches[0].clientY;
-    var touchYDelta = touchY - lastTouchY;
-    lastTouchY = touchY;
-    if (preventPullToRefresh) {
-        // To suppress pull-to-refresh it is sufficient to preventDefault the first overscrolling touchmove.
-        preventPullToRefresh = false;
-        if (touchYDelta > 0) {
-            e.preventDefault();
-            return;
-        }
-    }
+    // var touchY = e.originalEvent.touches[0].clientY;
+    // var touchYDelta = touchY - lastTouchY;
+    // lastTouchY = touchY;
+    // if (preventPullToRefresh) {
+    //     // To suppress pull-to-refresh it is sufficient to preventDefault the first overscrolling touchmove.
+    //     preventPullToRefresh = false;
+    //     if (touchYDelta > 0) {
+    //         e.preventDefault();
+    //         return;
+    //     }
+    // }
+    e.preventDefault();
 });
